79849dabb154609b479c733a5b2d8247d3630f32
王继魁 <wangjikui81@sina.com>
2020-11-19 16:27:57 +0800

    添加flasky_blog邮箱确认

---
+++ flasky_blog_确认邮件/app/auth/views.py
@@ -0,0 +1,77 @@
+from flask import render_template, redirect, request, url_for, flash
+from flask_login import login_user, logout_user, login_required, current_user
+from . import auth
+from ..models import User
+from .. import db
+from .forms import LoginForm, RegistrationForm
+from ..email import send_email
+
+@auth.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        print(User.query.all()[0].email)
+        if user is not None and user.verify_password(form.password.data):
+            login_user(user, form.remember_me.data)
+            next = request.args.get('next')
+            if next is None or not next.startswith('/'):
+                next = url_for('main.index')    
+            return redirect(next)
+        flash('用户名或密码错误')
+    return render_template('auth/login.html', form=form)
+
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash('你已经退出了！')
+    return redirect(url_for('main.index'))
+
+@auth.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(email=form.email.data, username=form.username.data, password=form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        # flash('你可以登录了！', 'success')
+        send_email(current_user.email, '确认账户', 'auth/email/confirm', user=user, token=token) 
+        flash('确认邮件已经发送到您的邮箱', 'success')
+        return redirect(url_for('auth.login'))
+    return render_template('auth/register.html', form=form)
+
+@auth.route('/confirm/<token>') 
+@login_required
+def confirm(token):
+    if current_user.confirmed:
+        return redirect(url_for('main.index'))
+    if current_user.confirm(token):
+        db.session.commit()
+        flash('你已经确认了你的邮件!', 'success')
+    else:
+        flash('确认链接非法与已经过期', 'warning')
+    return redirect(url_for('main.index'))
+
+@auth.before_app_request 
+def before_request():
+    if current_user.is_authenticated \
+            and not current_user.confirmed \
+            and request.blueprint != 'auth' \
+            and request.endpoint != 'static':
+        return redirect(url_for('auth.unconfirmed'))
+
+@auth.route('/unconfirmed')
+def unconfirmed():
+    if current_user.is_anonymous or current_user.confirmed: 
+        return redirect(url_for('main.index'))
+    return render_template('auth/unconfirmed.html')
+
+@auth.route('/confirm')
+@login_required
+def resend_confirmation():
+    token = current_user.generate_confirmation_token() 
+    send_email(current_user.email, '确认账户', 'auth/email/confirm', user=current_user, token=token) 
+    flash('确认邮件已经发送到您的邮箱', 'info')
+    return redirect(url_for('main.index'))
+
